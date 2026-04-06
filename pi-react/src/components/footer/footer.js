65e24aa96441baee0c10bc8dc50a709b491857a6
ldaud-lab <ldaud@udesa.edu.ar>
2026-04-06 16:23:18 -0300

    header y footer

---
+++ pi-react/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './footer.css'
+function Footer() {
+    return (
+        <footer className="alert alert-primary mt-4 text-center">
+            <p className="mb-0">Lucas Daud | Mauro Dagnino | Olivia De Diego</p>
+        </footer>
+    );
+}
+
+export default Footer;
